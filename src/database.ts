67521249740fa8d313c69fb966ce6fb35b580b9a
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2019-11-24 15:48:18 +0800

    allow the underlying database to be accessed

--- src/database.ts
+++ src/database.ts
@@ -3,19 +3,23 @@ import { Plugin } from 'vuex';
 
 
 export class ORMDatabase {
-
-    private static _ormDatabase = new VuexORM.Database();
-    private static _installed = <typeof Model[]>[];
+    private static _ormDatabase?: Database = undefined;
+    private static _installed: (typeof Model)[] = [];
 
     public static install(): Plugin<any> {
-        return VuexORM.install(ORMDatabase._ormDatabase);
+        return VuexORM.install(this.ormDatabase);
     }
 
-    public static registerEntity(model: typeof Model) {
+    public static registerEntity(model: typeof Model): void {
         if (this._installed.indexOf(model) !== -1) {
             console.error(`Unable to register entity ${model.name}.  Entity already registered.`)
             return;
         }
-        ORMDatabase._ormDatabase.register(model);
+        this.ormDatabase.register(model);
+    }
+
+    public static get ormDatabase(): Database {
+        if (!this._ormDatabase) this._ormDatabase = new Database();
+        return this._ormDatabase
     }
 }
