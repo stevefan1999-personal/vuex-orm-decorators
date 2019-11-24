a2ecf0c2048cb42e9435f7b1a4e9a0b98e35b82c
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2019-11-24 15:46:36 +0800

    remove geneated data

--- dist/types/database.d.ts
+++ dist/types/database.d.ts
@@ -1,8 +1,9 @@
-import { Model } from '@vuex-orm/core';
+import { Model, Database } from '@vuex-orm/core';
 import { Plugin } from 'vuex';
 export declare class ORMDatabase {
-    private static _ormDatabase;
+    private static _ormDatabase?;
     private static _installed;
     static install(): Plugin<any>;
     static registerEntity(model: typeof Model): void;
+    static readonly ormDatabase: Database;
 }
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,3 +1,6 @@
+import { Attribute, Model } from '@vuex-orm/core';
+import Mutator from '@vuex-orm/core/lib/attributes/contracts/Mutator';
 export { OrmModel } from './model';
 export { ORMDatabase } from './database';
 export { AttrField, BelongsToField, BelongsToManyField, BooleanField, Field, HasManyByField, HasManyField, HasManyThroughField, HasOneField, IncrementField, MorphManyField, MorphOneField, MorphToField, MorphToManyField, MorphedByManyField, NumberField, PrimaryKey, StringField } from './attributes';
+export { Mutator, Attribute, Model };
