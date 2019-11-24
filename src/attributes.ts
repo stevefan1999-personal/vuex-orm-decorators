0003335f346c4aaa1b88e85923e8a652f22e191e
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2019-11-24 15:48:01 +0800

    implement lazy generation

--- src/attributes.ts
+++ src/attributes.ts
@@ -1,7 +1,10 @@
 import { Model, Attribute } from '@vuex-orm/core';
 import Mutator from '@vuex-orm/core/lib/attributes/contracts/Mutator';
 
+type FunctorOrValue<T> = (() => T) | T
 
+const unwrapFunctorOrValue = <T>(functorOrValue: FunctorOrValue<T>): T =>
+    ((functorOrValue instanceof Function && functorOrValue()) || functorOrValue) as T
 
 /**
  * Sets the property as the primary key of the model
@@ -16,10 +19,10 @@ export function PrimaryKey() {
  * Adds the property as a model field
  * @param fieldType The field attribute
  */
-export function Field(fieldType: Attribute) {
+export function Field(fieldTypeFn: () => Attribute) {
     return (target: Object, propertyName: string | symbol): void => {
         (target.constructor as any)._fields = (target.constructor as any)._fields || {};
-        (target.constructor as any)._fields[propertyName] = fieldType;
+        (target.constructor as any)._fields[propertyName] = fieldTypeFn;
     };
 }
 
@@ -27,39 +30,39 @@ export function Field(fieldType: Attribute) {
  * Adds the property as a string typed field
  * @param defaultValue The default value for the field (if undefined the default will be '')
  */
-export function StringField(defaultValue?: string) {
-    return Field(Model.string(defaultValue || ''));
+export function StringField(defaultValue?: FunctorOrValue<string>) {
+    return Field(() => Model.string(unwrapFunctorOrValue(defaultValue) || ''));
 }
 
 /**
  * Adds the property as an incremental field
  */
 export function IncrementField() {
-    return Field(Model.increment());
+    return Field(() => Model.increment());
 }
 
 /**
  * Adds the property as a generic attribute field
- * @param defaultValue The default value for the field (if undiefine dthe default will be '')
+ * @param defaultValue The default value for the field (if undefined dthe default will be '')
  */
-export function AttrField(defaultValue?: any) {
-    return Field(Model.attr(defaultValue));
+export function AttrField<T>(defaultValue?: FunctorOrValue<T>) {
+    return Field(() => Model.attr(unwrapFunctorOrValue(defaultValue) || ''));
 }
 
 /**
  * Adds the property as a number typed field
  * @param defaultValue The default value for the field (if undefined the default will be 0)
  */
-export function NumberField(defaultValue?: number) {
-    return Field(Model.number(defaultValue || 0));
+export function NumberField(defaultValue?: FunctorOrValue<number>) {
+    return Field(() => Model.number(unwrapFunctorOrValue(defaultValue) || 0));
 }
 
 /**
  * Adds the property as a boolean typed field
  * @param defaultValue The default value for the field (if undefined the default will be FALSE)
  */
-export function BooleanField(value: any, mutator?: Mutator<boolean | null>) {
-    return Field(Model.boolean(value, mutator));
+export function BooleanField(value: FunctorOrValue<any>, mutator?: Mutator<boolean | null>) {
+    return Field(() => Model.boolean(unwrapFunctorOrValue(value) || false, mutator));
 }
 
 /**
@@ -68,8 +71,8 @@ export function BooleanField(value: any, mutator?: Mutator<boolean | null>) {
  * @param foreignKey The foreign key of the related model
  * @param localKey The local key on the parent model
  */
-export function HasManyField(related: typeof Model | string, foreignKey: string, localKey?: string) {
-    return Field(Model.hasMany(related, foreignKey, localKey));
+export function HasManyField(related: FunctorOrValue<typeof Model | string>, foreignKey: FunctorOrValue<string>, localKey?: FunctorOrValue<string>) {
+    return Field(() => (Model.hasMany as any)(...[related, foreignKey, localKey].map(unwrapFunctorOrValue)));
 }
 
 /**
@@ -78,8 +81,8 @@ export function HasManyField(related: typeof Model | string, foreignKey: string,
  * @param foreignKey The foreign key of the related model
  * @param localKey The local key on the parent model
  */
-export function HasOneField(related: typeof Model | string, foreignKey: string, localKey?: string) {
-    return Field(Model.hasOne(related, foreignKey, localKey));
+export function HasOneField(related: FunctorOrValue<typeof Model | string>, foreignKey: FunctorOrValue<string>, localKey?: FunctorOrValue<string>) {
+    return Field(() => (Model.hasOne as any)(...[related, foreignKey, localKey].map(unwrapFunctorOrValue)));
 }
 
 /**
@@ -88,39 +91,67 @@ export function HasOneField(related: typeof Model | string, foreignKey: string, 
  * @param foreignKey The foreign key of this model
  * @param ownerKey The key on the parent model
  */
-export function BelongsToField(parent: typeof Model | string, foreignKey: string, ownerKey?: string) {
-    return Field(Model.belongsTo(parent, foreignKey, ownerKey));
+export function BelongsToField(parent: FunctorOrValue<typeof Model | string>, foreignKey: FunctorOrValue<string>, ownerKey?: FunctorOrValue<string>) {
+    return Field(() => (Model.belongsTo as any)(...[parent, foreignKey, ownerKey].map(unwrapFunctorOrValue)));
 }
 
-export function HasManyByField(parent: typeof Model | string, foreignKey: string, ownerKey?: string) {
-    return Field(Model.hasManyBy(parent, foreignKey, ownerKey));
+export function HasManyByField(parent: FunctorOrValue<typeof Model | string>, foreignKey: FunctorOrValue<string>, ownerKey?: FunctorOrValue<string>) {
+    return Field(() => (Model.hasManyBy as any)(...[parent, foreignKey, ownerKey].map(unwrapFunctorOrValue)));
 }
 
-export function HasManyThroughField(related: typeof Model | string, through: typeof Model | string, firstKey: string, secondKey: string, localKey?: string, secondLocalKey?: string) {
-    return Field(Model.hasManyThrough(related, through, firstKey, secondKey, localKey, secondLocalKey));
+export function HasManyThroughField(
+    related: FunctorOrValue<typeof Model | string>, 
+    through: FunctorOrValue<typeof Model | string>, 
+    firstKey: FunctorOrValue<string>, secondKey: FunctorOrValue<string>, 
+    localKey?: FunctorOrValue<string>, secondLocalKey?: FunctorOrValue<string>
+) {
+    return Field(() => (Model.hasManyThrough as any)(...[related, through, firstKey, secondKey, localKey, secondLocalKey].map(unwrapFunctorOrValue)));
 }
 
-export function BelongsToManyField(related: typeof Model | string, pivot: typeof Model | string, foreignPivotKey: string, relatedPivotKey: string, parentKey?: string, relatedKey?: string) {
-    return Field(Model.belongsToMany(related, pivot, foreignPivotKey, relatedPivotKey, parentKey, relatedKey));
+export function BelongsToManyField(
+    related: FunctorOrValue<typeof Model | string>, 
+    pivot: FunctorOrValue<typeof Model | string>, 
+    foreignPivotKey: FunctorOrValue<string>, relatedPivotKey: FunctorOrValue<string>,
+    parentKey?: FunctorOrValue<string>, relatedKey?: FunctorOrValue<string>
+) {
+    return Field(() => (Model.belongsToMany as any)(...[related, pivot, foreignPivotKey, relatedPivotKey, parentKey, relatedKey].map(unwrapFunctorOrValue)));
 }
 
-export function MorphToField(id: string, type: string) {
-    return Field(Model.morphTo(id, type));
+export function MorphToField(
+    id: FunctorOrValue<string>, type: FunctorOrValue<string>
+) {
+    return Field(() => (Model.morphTo as any)(...[id, type].map(unwrapFunctorOrValue)));
 }
 
-export function MorphOneField(related: typeof Model | string, id: string, type: string, localKey?: string) {
-    return Field(Model.morphOne(related, id, type, localKey));
+export function MorphOneField(
+    related: FunctorOrValue<typeof Model | string>, 
+    id: FunctorOrValue<string>, type: FunctorOrValue<string>, localKey?: FunctorOrValue<string>
+) {
+    return Field(() => (Model.morphOne as any)(...[related, id, type, localKey].map(unwrapFunctorOrValue)));
 }
 
-export function MorphManyField(related: typeof Model | string, id: string, type: string, localKey?: string) {
-    return Field(Model.morphMany(related, id, type, localKey));
+export function MorphManyField(
+    related: FunctorOrValue<typeof Model | string>, 
+    id: FunctorOrValue<string>, type: FunctorOrValue<string>, localKey?: FunctorOrValue<string>
+) {
+    return Field(() => (Model.morphMany as any)(...[related, id, type, localKey].map(unwrapFunctorOrValue)));
 }
 
-export function MorphToManyField(related: typeof Model | string, pivot: typeof Model | string, relatedId: string, id: string, type: string, parentKey?: string, relatedKey?: string) {
-    return Field(Model.morphToMany(related, pivot, relatedId, id, type, parentKey, relatedKey));
+export function MorphToManyField(
+    related: FunctorOrValue<typeof Model | string>, 
+    pivot: FunctorOrValue<typeof Model | string>, 
+    relatedId: FunctorOrValue<string>, id: FunctorOrValue<string>, type: FunctorOrValue<string>, 
+    parentKey?: FunctorOrValue<string>, relatedKey?: FunctorOrValue<string>
+) {
+    return Field(() => (Model.morphToMany as any)(...[related, pivot, relatedId, id, type, parentKey, relatedKey].map(unwrapFunctorOrValue)));
 }
 
-export function MorphedByManyField(related: typeof Model | string, pivot: typeof Model | string, relatedId: string, id: string, type: string, parentKey?: string, relatedKey?: string) {
-    return Field(Model.morphedByMany(related, pivot, relatedId, id, type, parentKey, relatedKey));
+export function MorphedByManyField(
+    related: FunctorOrValue<typeof Model | string>, 
+    pivot: FunctorOrValue<typeof Model | string>, 
+    relatedId: FunctorOrValue<string>, id: FunctorOrValue<string>, type: FunctorOrValue<string>, 
+    parentKey?: FunctorOrValue<string>, relatedKey?: FunctorOrValue<string>
+) {
+    return Field(() => (Model.morphedByMany as any)(...[related, pivot, relatedId, id, type, parentKey, relatedKey].map(unwrapFunctorOrValue)));
 }
 
